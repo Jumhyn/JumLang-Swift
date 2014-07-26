a08b206a12c316f1a56438f991318d3930bc9cb5
Freddy Kellison-Linn <fred.kl@me.com>
2014-07-25 17:20:54 -0700

    Initial Commit

---
+++ SwiftCompiler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftCompiler
+//
+//  Created by Freddy Kellison-Linn on 7/25/14.
+//  Copyright (c) 2014 Jumhyn. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
