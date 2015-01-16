a93c2eb64de1ba0557483891859053a46d24736c
Frederick Kellison-Linn <fred.kl@me.com>
2015-01-15 23:08:12 -0800

    'get' and 'set' code for identifiers and prototypes

--- SwiftCompiler/Scope.swift
+++ SwiftCompiler/Scope.swift
@@ -10,7 +10,8 @@ import Foundation
 
 class Scope {
     var symTable: [Token : Identifier] = [:]
-    var previousScope: Scope
+    var funcTable: [Token : Prototype] = [:]
+    var previousScope: Scope?
 
     unowned var globalScope: Scope
 
@@ -18,8 +19,57 @@ class Scope {
         self.init(previousScope: previousScope, globalScope: self)
     }
 
+    convenience init() {
+        self.init(globalScope: self)
+    }
+
+    init(globalScope: Scope) {
+        self.globalScope = globalScope
+    }
+
     init(previousScope: Scope, globalScope: Scope) {
         self.previousScope = previousScope
         self.globalScope = globalScope
+    }
+
+    func identifier(forToken token: Token) -> Identifier! {
+        var currentScope = Scope(previousScope: self, globalScope: globalScope)
+        while let currentScope = currentScope.previousScope {
+            if let ret = symTable[token] {
+                return ret
+            }
+        }
+        if let ret = globalScope.symTable[token] {
+            return ret
+        }
+        else if let proto = self.prototype(forToken: token) {
+            return proto.id
+        }
+        return nil
+    }
+
+    func set(identifier id: Identifier, forToken token: Token) {
+        if let exists = symTable[token] {
+            //TODO: error handling...
+        }
+        else {
+            symTable[token] = id
+        }
+    }
+
+    func prototype(forToken token: Token) -> Prototype! {
+        if let proto = globalScope.funcTable[token] {
+            return proto
+        }
+        return nil
+    }
+
+    func set(prototype proto: Prototype, forToken token: Token) {
+        if let exists = globalScope.funcTable[token] {
+            //TODO: error handling...
+        }
+        else {
+            globalScope.funcTable[token] = proto
+        }
     }
 }
